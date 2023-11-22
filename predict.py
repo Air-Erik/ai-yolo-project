ed7d692ffc7f0d67ec15c388adddf84821610e69
Erik Air <airerik@yandex.ru>
2023-11-22 16:13:01 +0300

    Исправлены замечания линтера

--- predict.py
+++ predict.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 from PIL import Image
 import cv2
 
-#Загрузка модели для пердсказания
+# Загрузка модели для пердсказания
 model = YOLO("/runs/detect/train/weights/best.pt")
--- train.py
+++ train.py
@@ -1,11 +1,10 @@
-import torch
 from ultralytics import YOLO
 
-#Объявление модели
+# Объявление модели
 model = YOLO("yolov5s.yaml")
 
-#Обучение модели на наборе данных
+# Обучение модели на наборе данных
 result = model.train(data='datasets/test_1v/data.yaml', epochs=10, imgsz=640)
 
-#Оценка качества тренировки модели на проверочном сете
+# Оценка качества тренировки модели на проверочном сете
 result = model.val()
