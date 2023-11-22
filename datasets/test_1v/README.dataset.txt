b0f6b5e8e19168501b9a7d56db452dfe8e639a58
Erik Air <airerik@yandex.ru>
2023-11-22 16:01:29 +0300

    Создал скрипт для обучения нейросети

---
+++ datasets/test_1v/README.dataset.txt
@@ -0,0 +1,6 @@
+# Колодцы > 2023-11-17 3:22pm
+https://universe.roboflow.com/project-wwqou/-axlw0
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train.py
@@ -0,0 +1,11 @@
+import torch
+from ultralytics import YOLO
+
+#Объявление модели
+model = YOLO("yolov5s.yaml")
+
+#Обучение модели на наборе данных
+result = model.train(data='datasets/test_1v/data.yaml', epochs=10, imgsz=640)
+
+#Оценка качества тренировки модели на проверочном сете
+result = model.val()
