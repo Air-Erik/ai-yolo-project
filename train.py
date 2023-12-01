9cc38281356224f3394307d6d39ec683f1a7a2fb
AirErik <airerik@yandex.ru>
2023-12-01 11:49:00 +0300

    Рабочая модель

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
-from clearml import Task
+import clearml
 
-task = Task.init(project_name="my project", task_name="my task")
+clearml.browser_login()
 
 # Объявление модели
-model = YOLO("yolov5s.yaml")
+model = YOLO("yolov5m.pt")
 
 # Обучение модели на наборе данных
 result = model.train(data='datasets/test_1v/data.yaml', epochs=100, imgsz=640)
