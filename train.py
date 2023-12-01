98a3ffaef3707ae9b953578f19b2385eacf67fa7
AirErik <airerik@yandex.ru>
2023-12-01 11:28:45 +0300

    Добавил clearML

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
+from clearml import Task
+
+task = Task.init(project_name="my project", task_name="my task")
 
 # Объявление модели
 model = YOLO("yolov5s.yaml")
 
 # Обучение модели на наборе данных
-result = model.train(data='datasets/test_1v/data.yaml', epochs=30, imgsz=640)
+result = model.train(data='datasets/test_1v/data.yaml', epochs=100, imgsz=640)
 
 # Оценка качества тренировки модели на проверочном сете
 result = model.val()
