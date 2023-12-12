9758ae3e46c411cfe814dc53e30d15dc2e3e1269
Air Erik <airerik@yandex.ru>
2023-12-12 15:14:08 +0300

    Изменен принцип приема исходных изображений

--- predict.py
+++ predict.py
@@ -2,14 +2,23 @@ from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 import pandas as pd
+import os
+import glob
+
+# Название папки с обученными весами (название используется для вывода результата)
+custom_weights = 'train6'
+# путь к папке с изображениями для тестирования
+pth_test = 'test/AutoCAD_Topo_v7/'
+
 
 # Загрузка модели для пердсказания
-custom_model = 'train6'
-model = YOLO(f"runs/detect/{custom_model}/weights/best.pt")
+model = YOLO(f"runs/detect/{custom_weights}/weights/best.pt")
 
-source = ['test/AutoCAD_Topo_v7/test_1_0_0.jpg', 'test/AutoCAD_Topo_v7/test_1_0_1.jpg',
-          'test/AutoCAD_Topo_v7/test_1_0_0x2.jpg', 'test/AutoCAD_Topo_v7/test_1_0_0x1.5.jpg',
-          'test/AutoCAD_Topo_v7/test_1_0_0x2(1280).jpg']
+# Список тестовых изображений
+source = []
+for file in glob.glob(pth_test + '*'):
+    source.append(file)
+print(source)
 
 # Предсказание. Параметр conf определяет достоверный порог вероятности при котором засчитывается обнаружение
 results = model(source, conf=0.65)
@@ -22,7 +31,7 @@ for r in results:
     # Сохранение картинки с рамками захвата
     im_array = r.plot()
     im = Image.fromarray(im_array[..., ::-1])
-    im.save(f'result/{custom_model}/result{i}.jpg')
+    im.save(f'result/{custom_weights}/result{i}.jpg')
     
     # Конвертация результатов из tensor в numpy
     frames = r.boxes.xyxy.cpu().numpy()
@@ -32,7 +41,7 @@ for r in results:
     # Создание таблицы pandas с последующей выгрузкой в CSV
     df = pd.DataFrame(frames, columns=['x1', 'y1', 'x2', 'y2'])
     df['percent'], df['class'] = pd.DataFrame(percent), pd.DataFrame(clas)
-    df.to_csv(f'result/{custom_model}/database{i}.csv')
+    df.to_csv(f'result/{custom_weights}/data_{i}.csv')
     
     # Печать таблицы
     print(df.head())
