2c31abc0b5a0b44e2a78e2a36f06f1ad8f7223e9
Air Erik <airerik@yandex.ru>
2023-12-13 12:15:09 +0300

    Исправлены замечания линтера

--- train_clearML.py
+++ train_clearML.py
@@ -2,34 +2,41 @@ from clearml import Task
 from ultralytics import YOLO
 import torch
 
+
 def main():
-    #Проверка работы CUDA и вычисления на GPU
+    # Проверка работы CUDA и вычисления на GPU
     print(torch.cuda.is_available())
     print(torch.cuda.device_count())
     print(torch.cuda.current_device())
     print(torch.cuda.device(0))
     print(torch.cuda.get_device_name(0))
-    
-    # ClearML; Создание объекта задачи для clearml, описывает проект и название текущей сессии
+
+    # ClearML; Создание объекта задачи для clearml, описывает проект и
+    # название текущей сессии
     task = Task.init(
         project_name="AutoCAD",
         task_name="yolov8l"
     )
-    
+
     # ClearML; Определение модели на которой будет происходить обучение
     model_variant = "yolov8l"
     task.set_parameter("model_variant", model_variant)
-    
+
     # YOLO; Объявление модели
     model = YOLO(f'{model_variant}.pt')
-    
+
     # ClearML; Устанавка параметров обучения, передаются в функцию обучения
-    args = dict(data='datasets/AutoCAD_Topo_v7/data.yaml', epochs=300, imgsz=640, freeze=10)
+    args = dict(data='datasets/AutoCAD_Topo_v7/data.yaml',
+                epochs=300,
+                imgsz=640,
+                freeze=10)
     task.connect(args)
-    
+
     # YOLO; Обучение модели на наборе данных
     result = model.train(**args)
 
-# Необходимое условие для многопоточности. Без данной конструкции не возможен параллелизм
+
+# Необходимое условие для многопоточности. Без данной конструкции не возможен
+# параллелизм
 if __name__ == '__main__':
     main()
