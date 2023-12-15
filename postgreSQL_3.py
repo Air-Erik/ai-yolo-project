0e79e0af8d163219da5319bebd4540f49f3e9e8d
Air Erik <airerik@yandex.ru>
2023-12-15 16:10:12 +0300

    Разобрался какой тип данных нужно передавать в SQL запрос

--- postgreSQL_3.py
+++ postgreSQL_3.py
@@ -1,14 +1,15 @@
 import psycopg
 
-with psycopg.connect('dbname=AI_Database user=ayrapetov_es \
+with psycopg.connect('dbname=ai_database user=ayrapetov_es \
 password=1111') as conn:
     with conn.cursor() as cur:
 
         cur.execute(
-            'INSERT INTO new_table (class, class_id, x_1, y_1, x_2, y_2, \
-            percent) VALUES (%s, %s, %s, %s, %s, %s, %s)',
-            ('well', 10, 0.1, 1.1, 20.1, 30.1, 80.25))
+            'INSERT INTO train (class, class_id, x_1, y_1, x_2, y_2, \
+            percent, file_name) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+            ('well', 10, 0.1, 1.1, 20.1, 30.1, 80.25, 'erik'))
+        cur.close()
 
-        conn.commit()
+    conn.commit()
 
 print('yes')
--- predict.py
+++ predict.py
@@ -2,12 +2,13 @@ from ultralytics import YOLO
 from PIL import Image
 import pandas as pd
 import os
+import psycopg
 
 # Название папки с обученными весами (название используется для вывода
 # результата)
 custom_weights = 'train6'
-# Путь к папке с изображениями для тестирования
-pth_test = 'test/AutoCAD_Topo_v7/'
+# Путь к папке с изображениями для тестирования 'test/AutoCAD_Topo_v7/'
+pth_test = 'test/test_1v/'
 
 # Загрузка модели для пердсказания
 model = YOLO(f"runs/detect/{custom_weights}/weights/best.pt")
@@ -22,6 +23,7 @@ for dirpath, dirnames, filenames in os.walk(pth_test):
         source.append(os.path.join(dirpath, filename))
         file_names.append(os.path.splitext(filename)[0])
 
+print(file_names)
 # Предсказание. Параметр conf определяет достоверный порог вероятности при
 # котором засчитывается обнаружение
 results = model(source, conf=0.70)
@@ -48,6 +50,19 @@ for r in results:
     # Создание таблицы pandas с последующей выгрузкой в CSV
     df = pd.DataFrame(frames, columns=['x1', 'y1', 'x2', 'y2'])
     df['percent'], df['class'] = pd.DataFrame(percent), pd.DataFrame(clas)
-    df.to_csv(f'result/{custom_weights}/data_{file_names[i]}.csv')
+    # df.to_csv(f'result/{custom_weights}/data_{file_names[i]}.csv')
+
+    # Запись в базу данных
+    with psycopg.connect('dbname=ai_database user=ayrapetov_es password=1111') as conn:
+        with conn.cursor() as cur:
+
+            for index, row in df.iterrows():
+                cur.execute(
+                    'INSERT INTO train (x_1, y_1, x_2, y_2, percent, file_name, class, class_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                    (float(row['x1']), float(row['y1']), float(row['x2']), float(row['y2']),
+                    float(row['percent']), file_names[i], 'none', float(row['class'])))
+            cur.close()
+
+        conn.commit()
 
     i += 1  # Увеличение счетчика
