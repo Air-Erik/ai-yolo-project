518c8fd8f8fce3b5dc7e18680396d5ff407fb95e
Air Erik <airerik@yandex.ru>
2023-12-15 16:48:05 +0300

    Удалил ненужный код. Все рабочие скрипты перенесены в основные файлы

--- postgreSQL_2.py
+++
@@ -1,3 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(dbname='AI_Database', user='ayrapetov_es', password='1111')
--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def main():
-    print(os.listdir(path='test/AutoCAD_Topo_v7/'))
-
-if __name__ == '__main__':
-    main()
